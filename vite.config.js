1253ea8cc7116d678427401c2509d5b5fc1dce89
Sunny <gurujii4545@gmail.com>
2024-10-20 09:08:35 +0530

    h

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    port:400,
+    port:5173
+},
   plugins: [react()],
 })
